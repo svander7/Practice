404b6829d7e8183f90c8dde63a20dd1f7cc6d3f6
Sonny Vanderhoeven <147276430+svander7@users.noreply.github.com>
2023-12-21 11:12:39 -0500

    Just playing

---
+++ src/com/coderscampus/random/Random.java
@@ -0,0 +1,15 @@
+package com.coderscampus.random;
+
+import java.util.Arrays;
+
+public class Random {
+
+	public static void main(String[] args) {
+		
+		String[] cars = {"Volvo", "BMW", "Ford", "Mazda", "Dodge", "Chevy", "Jeep"};
+		
+		for (String i : cars) {
+			System.out.println(i);
+		}
+	}
+}
